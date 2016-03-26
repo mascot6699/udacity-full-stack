87efdd238def0f1a67ec3f03309dbf4993941f2f
Umang Shukla <umang.shukla@kuliza.com>
2016-03-26 23:47:22 +0530

    Removes unnecssary coloumn from players table

--- Tournament Planner/vagrant/tournament/tournament.sql
+++ Tournament Planner/vagrant/tournament/tournament.sql
@@ -5,8 +5,7 @@
 
 
 CREATE TABLE players  ( id serial primary key,
-                        name varchar (25) not null,
-                        created_at timestamp default current_timestamp );
+                        name varchar (25) not null);
 
 CREATE TABLE matches  ( id serial primary key,
                         winner_id int,
