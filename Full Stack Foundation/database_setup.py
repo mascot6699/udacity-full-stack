bee2c2ab826348209cd7235789b8fe079a6bc707
Umang Shukla <umang.shukla@kuliza.com>
2016-03-29 22:42:12 +0530

    Adds the puppy model

--- Full Stack Foundation/database_setup.py
+++ Full Stack Foundation/database_setup.py
@@ -6,13 +6,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+
 Base = declarative_base()
+
 
 class Shelter(Base):
     """
-    Schema for shelter database
+    Schema for shelter table
     """
     __tablename__ = "shelter"
+
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     address = Column(String(500))
@@ -22,6 +25,18 @@ class Shelter(Base):
     website = Column(String(100))
 
 
+class Puppy(Base):
+    """
+    Schema for puppy table
+    """
+    __tablename__ = "puppy"
+
+    name = Column(String(100), nullable=False)
+    dob = Column(Date)
+    gender = Column(String(6))
+    weight = Column(Numeric)
+    shelter_id = Column(Integer, ForeignKey('shelter.id'))
+    shelter = relationship(Shelter)
 
 engine = create_engine('sqlite:///puppy.db')
 Base.metadata.create_all(engine)
