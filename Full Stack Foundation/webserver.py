fa56122700622ea2cd4e71410898b0d0533d75e0
Umang Shukla <umang.shukla@kuliza.com>
2016-04-02 15:31:59 +0530

    Creates a basic Webserver in python

---
+++ Full Stack Foundation/webserver.py
@@ -0,0 +1,33 @@
+"""
+Practice code for making own webserver
+"""
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+class WebServerHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path.endswith("/umang/"):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            message = "<html><body>Hello Umang!</body></html>"
+            self.wfile.write(message)
+            print message
+            return
+        else:
+            self.send_error(404, 'File Not Found: %s. Try hitting /umang/' % self.path)
+
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), WebServerHandler)
+        print "Web Server running on port %s" % port
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print " ^C entered, stopping web server...."
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
