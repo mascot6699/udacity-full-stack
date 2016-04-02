510810b8efa9e48a9c8d4b5339ed043ec934f0dd
Umang Shukla <umang.shukla@kuliza.com>
2016-04-02 15:56:00 +0530

    Adds the post handler to show message sent

--- Full Stack Foundation/webserver.py
+++ Full Stack Foundation/webserver.py
@@ -2,16 +2,26 @@
 Practice code for making own webserver
 """
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
 
 
 class WebServerHandler(BaseHTTPRequestHandler):
+
+    form = '''<form method='POST' enctype='multipart/form-data' action='/umang/'>
+              <h2>What would you like me to say?</h2>
+              <input name="message" type="text" >
+              <input type="submit" value="Submit">
+              </form>
+           '''
 
     def do_GET(self):
         if self.path.endswith("/umang/"):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            message = "<html><body>Hello Umang!</body></html>"
+            message = "<html><body>Hello Umang!"
+            message += WebServerHandler.form
+            message += "</body></html>"
             self.wfile.write(message)
             print message
             return
@@ -20,13 +30,37 @@ class WebServerHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            message = ""
-            message += "<html><body> &#161 Hola ! </body></html>"
+            message = "<html><body> &#161 Hola !"
+            message += WebServerHandler.form
+            message += "</body></html>"
             self.wfile.write(message)
             print message
             return
         else:
             self.send_error(404, 'File Not Found: %s. Try hitting /umang/ or /hola/' % self.path)
+
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            ctype, pdict = cgi.parse_header(
+                self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':  # checks is form data is recieved
+                fields = cgi.parse_multipart(self.rfile, pdict)  # collects all fields in a form
+                messagecontent = fields.get('message')  # store value of message field and store in an array
+                message = "<html><body>"
+                message += " <h2> Okay, how about this: </h2>"
+                message += "<h1> %s </h1>" % messagecontent[0]
+                message += WebServerHandler.form
+                message += "</body></html>"
+                self.wfile.write(message)
+                print message
+            else:
+                self.send_error(404, 'File Not Found: %s. Try hitting /umang/ or /hola/ or '
+                                     'hit with multipart/form-data ' % self.path)
+        except:
+            pass
 
 
 def main():
