161716275bc0f0167a256cb010947fdc7a8bdc18
Umang Shukla <umang.shukla@kuliza.com>
2016-04-05 10:13:29 +0530

    Adds the new restaurant add page

--- Full Stack Foundation/Restaurants/webserver.py
+++ Full Stack Foundation/Restaurants/webserver.py
@@ -3,7 +3,7 @@ Practice code for making own webserver
 """
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
-import views
+import views, utils
 
 
 class WebServerHandler(BaseHTTPRequestHandler):
@@ -28,48 +28,23 @@ class WebServerHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            message = "<html><body> To create a new restaurant fill this form and click save <br><br><br>"
-            message += WebServerHandler.form
-            message += "</body></html>"
+            message = views.get_create_new_restaurant_form()
             self.wfile.write(message)
-            print message
-            return
-
-        if self.path.endswith("/restaurants/new"):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            message = "<html><body> To create a new restaurant fill this form and click save <br><br><br>"
-            message += WebServerHandler.form
-            message += "</body></html>"
-            self.wfile.write(message)
-            print message
             return
         else:
             self.send_error(404, 'File Not Found: %s. Try hitting /restaurants' % self.path)
 
     def do_POST(self):
-        try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':  # checks is form data is recieved
-                fields = cgi.parse_multipart(self.rfile, pdict)  # collects all fields in a form
-                messagecontent = fields.get('message')  # store value of message field and store in an array
-                message = "<html><body>"
-                message += " <h2> Okay, how about this: </h2>"
-                message += "<h1> %s </h1>" % messagecontent[0]
-                message += WebServerHandler.form
-                message += "</body></html>"
-                self.wfile.write(message)
-                print message
-            else:
-                self.send_error(404, 'File Not Found: %s. Try hitting /umang/ or /hola/ or '
-                                     'hit with multipart/form-data ' % self.path)
-        except:
-            pass
+        if self.path.endswith("/restaurants/new"):
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('name')
+                utils.create_restaurant(name=messagecontent[0])
+                self.send_response(301)
+                self.send_header('Content-type', 'text/html')
+                self.send_header('Location', '/restaurants')
+                self.end_headers()
 
 
 def main():
