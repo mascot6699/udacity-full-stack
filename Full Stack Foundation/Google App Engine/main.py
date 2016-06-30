ec220f030ad8316025db9c8db6c6fe69d1625376
Umang Shukla <umang.shukla@kuliza.com>
2016-06-30 09:36:38 +0530

    Shopping list demo complete

---
+++ Full Stack Foundation/Google App Engine/main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+
+import webapp2
+import jinja2
+
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+    """
+    Jinja template handler
+    """
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainHandler(Handler):
+    """
+    Contains form for submission
+    """
+    def get(self):
+        """
+        Shows form back.
+        """
+        items = self.request.get_all("food")
+        self.render("shopping_list.html", items=items)
+
+
+app = webapp2.WSGIApplication(
+    [('/', MainHandler),
+    ], debug=True)
