2d945b18bd84f3745df599ce0bec6f8fc3dc373e
Umang Shukla <umang.shukla@kuliza.com>
2016-07-05 20:54:23 +0530

    Adds the ascii art forum page

--- Full Stack Foundation/Google App Engine/main.py
+++ Full Stack Foundation/Google App Engine/main.py
@@ -141,8 +141,34 @@ class IndexPageHandler(Handler):
         self.render('index.html')
 
 
+class AsciiForumHandler(Handler):
+    """
+    ASCII art forum.
+    """
+    def render_ascii(self, title="", art="", error=""):
+        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC LIMIT 10")
+        self.render('ascii.html', title=title, art=art, error=error, 
+            arts=arts)
+
+    def get(self):
+        self.render_ascii()
+
+    def post(self):
+        title = self.request.get('title')
+        art = self.request.get('art')
+
+        if title and art:
+            a = Art(title=title, art=art)
+            a.put()
+            self.redirect('/ascii/')
+        else:
+            error = "We need both a title and some artwork!"
+            self.render_ascii(title, art, error)
+
+
 app = webapp2.WSGIApplication(
     [('/', IndexPageHandler),
+     ('/ascii/', AsciiForumHandler),
      ('/shopping/list/', ShoppingListHandler),
      ('/rot13/', Rot13Handler),
      ('/welcome/', WelcomePage),
