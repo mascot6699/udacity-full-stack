0965c62a8b633a021fe3ea46f3674ce12361182c
Umang Shukla <umang.shukla@kuliza.com>
2016-07-13 20:20:44 +0530

    Adds User enitity

--- Multi User Blog/Multi User Blog/entities/entity.py
+++ Multi User Blog/Multi User Blog/entities/entity.py
@@ -1,3 +1,14 @@
 """
 This file will contain all the entities required for Blog app
 """
+from google.appengine.ext import db
+
+class User(db.Model):
+    """
+    Entity to store for User.
+    """
+    username = db.StringProperty(required=True)
+    password = db.StringProperty(required=True)  # hash of password will be stored
+    email = db.StringProperty()
+    fullname = db.StringProperty()
+    about = db.StringProperty()
