4beb451e97e1940dea33ee157d7767dd7a3442a1
Umang Shukla <umang.shukla@kuliza.com>
2016-07-13 20:41:22 +0530

    Adds Blog, Like and Comment entity

--- Multi User Blog/Multi User Blog/entities/entity.py
+++ Multi User Blog/Multi User Blog/entities/entity.py
@@ -12,3 +12,34 @@ class User(db.Model):
     email = db.StringProperty()
     fullname = db.StringProperty()
     about = db.StringProperty()
+
+
+class Post(db.Model):
+    """
+    Entity to store a Blog Post.
+    """
+    title = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    user = db.ReferenceProperty(User, required=True, collection_name='posts')
+    is_draft = db.BooleanProperty(default=False)
+    modified_at = db.DateTimeProperty(auto_now=True)
+    created_at = db.DateTimeProperty(auto_now_add=True)
+    published_at = db.DateTimeProperty(required=True)
+
+
+class Like(db.Model):
+    """
+    Store the like of a Blog Post
+    """
+    user = db.ReferenceProperty(User, required=True, collection_name='likes')
+    post = db.ReferenceProperty(Post, required=True, collection_name='likes')
+
+
+class Comment(db.Model):
+    """
+    Store the comment of a Blog Post.
+    """
+    post = db.ReferenceProperty(Post, required=True, collection_name='comments')
+    user = db.ReferenceProperty(User, required=True, collection_name='comments')
+    comment = db.StringProperty(required=True)
+    created_at = db.DateTimeProperty(auto_now_add=True)
