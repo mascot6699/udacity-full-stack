09f56d4f4dc0f48a4016fe276559c48a23fb87fc
Umang Shukla <umang.shukla@kuliza.com>
2016-07-30 12:08:55 +0530

    utils to create a user from login session and get users

---
+++ Full Stack Foundation/Restaurants/Flask/utils.py
@@ -0,0 +1,20 @@
+def create_user(login_session):
+    newUser = User(name=login_session['username'], 
+        email=login_session['email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def get_user_info(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def get_user_id(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
