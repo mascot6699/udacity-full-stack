4dc17841d087881d8bffc12ce83637bb143b9e1a
Umang Shukla <umang.shukla@kuliza.com>
2016-07-30 12:08:14 +0530

    Adds user table

--- Full Stack Foundation/Restaurants/Flask/database_setup.py
+++ Full Stack Foundation/Restaurants/Flask/database_setup.py
@@ -10,6 +10,15 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
 class Restaurant(Base):
     """
     Schema for restaurant table
@@ -19,6 +28,8 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     website = Column(String(100), nullable=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -41,6 +52,8 @@ class MenuItem(Base):
     price = Column(String(8))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -55,5 +68,5 @@ class MenuItem(Base):
             }
 
 
-engine = create_engine('sqlite:///restaurant.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 Base.metadata.create_all(engine)
