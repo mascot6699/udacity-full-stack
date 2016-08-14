9d5b52617f883c6852d6048d3a9bdd0fdfdd3270
Umang Shukla <umang.shukla@kuliza.com>
2016-08-14 11:31:27 +0530

    Catalog app enitities added

---
+++ Item Catalog/vagrant/database_setup.py
@@ -0,0 +1,74 @@
+"""
+Defines the database setup to make tables in db
+"""
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Numeric
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    """
+    Schema for category table
+    """
+    __tablename__ = "category"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """
+        Return object data in serialized format
+        """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id
+            }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250))
+    price = Column(String(8))
+    image = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """
+        Return object data in serialized format
+        """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'price': self.price,
+            'image': self.image,
+            'user_id': self.user_id
+            }
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
